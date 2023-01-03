1f73411a2aac92f90f9134a5f8fdfa41ce5567be
Taemienik <temmynik@gmil.com>
2023-01-02 23:41:22 -0800

    t3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 /*
  * main - prints the alphabet
- *
+ * Followed by a new line
  * Return: Always 0 (success)
  */
 int main(void)
 {
-	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-	int i;
+	char ch;
 
-	for (i = 0; i < 26; i++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		putchar(alp[i]);
+		putchar(ch);
 	}
 	putchar('\n');
 	return (0);
